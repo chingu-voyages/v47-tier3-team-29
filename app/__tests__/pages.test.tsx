791e8ad08d826dacc9eac386a1031eb1c1b7ff1d
fmtabbara <fmtabbara@hotmail.co.uk>
2024-01-22 21:23:14 +0000

    Add vitest configuration file, update GitHub Actions workflow, add test for pages, and update package.json

---
+++ app/__tests__/pages.test.tsx
@@ -0,0 +1,13 @@
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Page from '@/app/page'
+
+test('example', () => {
+  expect(true).toBe(true)
+})
+
+test('Page', () => {
+  render(<Page />)
+  const element = screen.queryByText('Heads up!')
+  expect(element).toBeTruthy()
+})
