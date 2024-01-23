e37b88a1c88c25e905b04533b43e5ac0c42db674
fmtabbara <fmtabbara@hotmail.co.uk>
2024-01-23 21:30:02 +0000

    set up db and prisma + add a test

--- app/__tests__/pages.test.tsx
+++ app/__tests__/pages.test.tsx
@@ -1,6 +1,7 @@
 import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Page from '@/app/page'
+import { PrismaClient } from '@prisma/client'
 
 test('example', () => {
   expect(true).toBe(true)
@@ -10,4 +11,14 @@ test('Page', () => {
   render(<Page />)
   const element = screen.queryByText('Heads up!')
   expect(element).toBeTruthy()
+})
+
+test('DB', async () => {
+  const prisma = new PrismaClient()
+  const user = await prisma.user.findUnique({
+    where: {
+      id: '84ca7613-c981-4171-9f5b-02b6c0ce76b2',
+    },
+  })
+  expect(user?.name).toBe('Fouad')
 })
