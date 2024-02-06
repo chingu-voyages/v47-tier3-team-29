6ff0fb080eb6786fc9d59b76e752384b919f8656
fmtabbara <fmtabbara@hotmail.co.uk>
2024-02-06 13:07:37 +0000

    fix next build

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import StrapLine from '@/components/StrapLine';
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <StrapLine />
+      <div className="font-semibold text-5xl tracking-tighter">
+        Welcome to next level{' '}
+        <span className="text-indigo-800">Project Management</span>.{' '}
+      </div>
     </main>
   );
 }
