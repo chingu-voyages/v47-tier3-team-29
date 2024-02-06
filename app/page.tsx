712c71eba78d719da73cf9a87a124dc95217f7eb
fmtabbara <fmtabbara@hotmail.co.uk>
2024-02-06 12:15:37 +0000

    fix test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import StrapLine from '@/components/StrapLine';
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col items-center justify-between p-24">
+      <StrapLine />
+    </main>
   );
 }
