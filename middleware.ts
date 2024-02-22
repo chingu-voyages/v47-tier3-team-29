2035ba0e3d218616840157a27c49e1630ba45c8a
Akashay Anand <akashayanand1@gmail.com>
2024-02-22 20:14:09 +0530

    home layout design

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,3 @@
 export { default } from 'next-auth/middleware';
 
-export const config = { matcher: ['/dashboard', '/tasks'] };
+export const config = { matcher: ['/home', '/dashboard', '/tasks'] };
