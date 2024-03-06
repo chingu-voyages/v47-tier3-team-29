4d154e25bd159cdd38a68b6c73d661363ee7a7e9
Akashay Anand <akashayanand1@gmail.com>
2024-03-07 02:53:10 +0530

    resolve lint error

--- components/WelcomeScreen/index.tsx
+++ components/WelcomeScreen/index.tsx
@@ -2,6 +2,7 @@ import Logo from "@/components/Logo";
 import { Button } from "@/components/ui/button"
 import { Mail } from "lucide-react";
 import Footer from "@/components/Footer";
+import Image from "next/image";
 
 export default function WelcomeScreen() {
   return (
@@ -28,7 +29,7 @@ export default function WelcomeScreen() {
 
       <section>
         <div className="w-full">
-          <img src="/ws-team.webp" alt="Group Image" className="w-full h-auto" />
+          <Image src="/ws-team.webp" alt="Group Image" layout="responsive" width={100} height={100} className="w-full h-auto" />
         </div>
       </section>
 
@@ -39,14 +40,14 @@ export default function WelcomeScreen() {
         <h2 className="text-3xl md:text-4xl font-bold mb-4">
           <span className="text-purple-600">breezy success!</span>
         </h2>
-        <p>We simplify management, you grow. We're responsive. Don’t wait, start today.</p>
+        <p>We simplify management, you grow. We&apos;re responsive. Don&apos;t wait, start today.</p>
       </section>
       
       <section>
         <div className="p-4">
           <div className="flex flex-col md:flex-row items-center justify-between gap-8">
             <div className="w-full md:w-1/2">
-              <img src="/ws-homeDesktop.webp" alt="home" className="max-w-full h-auto mx-auto" />
+              <Image src="/ws-homeDesktop.webp" alt="home" layout="responsive" width={100} height={100}  className="max-w-full h-auto mx-auto" />
             </div>
             <div className="w-full md:w-1/2">
               <h2>Streamline tasks, boost collaboration, and conquer deadlines effortlessly.</h2>
@@ -57,10 +58,10 @@ export default function WelcomeScreen() {
         <div className="p-4">
           <div className="flex flex-col md:flex-row items-center justify-between gap-8">
             <div className="w-full md:w-1/2">
-              <h2>We make management a breeze, so you can focus on what truly matters – growth, innovation, and enjoying the journey.</h2>
+              <h2>We make management a breeze, so you can focus on what truly matters - growth, innovation, and enjoying the journey.</h2>
             </div>
             <div className="w-full md:w-1/2">
-              <img src="/ws-project.webp" alt="home" className="max-w-full h-auto mx-auto" />
+              <Image src="/ws-project.webp" alt="home" layout="responsive" width={100} height={100} className="max-w-full h-auto mx-auto" />
             </div>
           </div>
         </div>
@@ -68,15 +69,15 @@ export default function WelcomeScreen() {
 
       <section className="text-center px-4 py-8">
         <h2 className="text-3xl md:text-4xl font-bold mb-4">
-          We simplify <span className="text-purple-600">management</span>, you <span className="text-purple-600">grow</span>. We're <span className="text-purple-600">responsive</span> Don’t wait, <span className="text-purple-600">start today.</span></h2>
+          We simplify <span className="text-purple-600">management</span>, you <span className="text-purple-600">grow</span>. We&apos;re <span className="text-purple-600">responsive</span> Don&apos;t wait, <span className="text-purple-600">start today.</span></h2>
       </section>
 
       <section className="flex flex-row justify-center items-center gap-4 p-4">
         <div>
-          <img src="/ws-iPad_Pro_2020.webp" alt="Image 1" className="max-w-full h-auto" />
+          <Image src="/ws-iPad_Pro_2020.webp" alt="tab screen" layout="responsive" width={100} height={100} className="max-w-full h-auto" />
         </div>
         <div>
-          <img src="/ws-iPhone_13.webp" alt="Image 2" className="max-w-full h-auto" />
+          <Image src="/ws-iPhone_13.webp" alt="phone screen" layout="responsive" width={100} height={100} className="max-w-full h-auto" />
         </div>
       </section>
 
@@ -84,8 +85,7 @@ export default function WelcomeScreen() {
         <div className="flex flex-col md:flex-row items-center justify-center gap-4 my-8">
           <p className="self-center">Get Started</p>
           <Button className="self-center">
-            <Mail />  Log In with Email
-          </Button>
+            <Mail />  Log In with Email  </Button>
         </div>
       </section>
       <div className="mt-20">&nbsp;</div>
